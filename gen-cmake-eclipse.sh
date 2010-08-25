2cfc9e899fda813e19ed691b3f8399b30e294f49
Ivan Pouzyrevsky <sandello@gmail.com>
2010-08-25 11:31:27 +0400

    Altered gen-cmake-* files to create build directory if required

--- gen-cmake-eclipse.sh
+++ gen-cmake-eclipse.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -e
 
+(test \! -d build && mkdir build)
 (cd build && cmake -DCMAKE_BUILD_TYPE=Release -G"Eclipse CDT4 - Unix Makefiles" ..)
--- gen-cmake-vs2008.bat
+++ gen-cmake-vs2008.bat
@@ -1,3 +1,4 @@
+md build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -G"Visual Studio 9 2008" ..
 pause
